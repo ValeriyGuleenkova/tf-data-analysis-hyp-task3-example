6888f1ff0ab59923748debf1bb423529d5d11194
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-11 23:16:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 215606022  # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+SGN_LVL = 0.09
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+    pval = ttest_ind(x, y, equal_var=False, alternative='less').pvalue
+    if pval < SGN_LVL:
+        return True
+    # true: выборки не равны
+    return False
