08e559767e18293df2e72b3064e723b4f7178410
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-11 23:52:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import permutation_test
 
 chat_id = 215606022  # Ваш chat ID, не меняйте название переменной
 
 SGN_LVL = 0.09
 
 def solution(x: np.array, y: np.array) -> bool:
-    pval = ttest_ind(np.log(x), np.log(y), equal_var=False, alternative='less').pvalue
+    pval = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis),
+                                            vectorized=True,
+                                            n_resamples=5000,
+                                            alternative='less').pvalue
     if pval < SGN_LVL:
         return True
     # true: выборки не равны
